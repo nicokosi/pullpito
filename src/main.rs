4a537de09810a8ad30091f5e534e0268e22a72d5
Nicolas Kosinski <nicokosi@yahoo.com>
2018-04-09 07:10:59 +0200

    Use Result instead of panic when parsing params

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Computing stats for GitHub repo {}", config.repo);
 }
 
@@ -11,15 +15,17 @@ struct Config {
     token: Option<String>
 }
 
-fn parse_config(args: &[String]) -> Config {
-    if args.len() < 1 {
-        panic!("Not enough arguments, expecting at least 1 argument");
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 1 {
+            return Err("Not enough arguments, expecting at least 1 argument");
+        }
+        let repo = args[1].clone();
+        let token = if args.len() == 2 {
+            Some(args[1].clone())
+        } else {
+            None
+        };
+        Ok(Config { repo, token })
     }
-    let repo = args[1].clone();
-    let token = if args.len() == 2 {
-        Some(args[1].clone())
-    } else {
-        None
-    };
-    Config { repo, token }
 }
