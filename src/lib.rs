8a7a925e3aa0459b7569184c40cf6c41554411e2
Nicolas Kosinski <nicolas.kosinski@vidal.fr>
2018-04-12 09:46:35 +0200

    GitHub request now returns HTTP 200! 🎉

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,29 @@ impl Config {
     }
 }
 
+pub fn github_events(config: Config) {
+    let mut core = Core::new().unwrap();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &core.handle()).unwrap())
+        .build(&core.handle());
+
+    let url = "https://api.github.com/repos/".to_string() + &config.repo + "/events?access_token="
+        + &config.token.unwrap_or("".to_string()) + "&page=1";
+    let mut req = Request::new(Method::Get, url.parse().unwrap());
+    req.headers_mut().set_raw("Accept", "application/vnd.github.v3+json");
+    req.headers_mut().set_raw("Host", "api.github.com");
+    req.headers_mut().set_raw("User-Agent", "pullpito/0.1.0");
+    println!("GitHub request: {:?}", &req);
+
+    let request = client
+        .request(req)
+        .map(|res| {
+            println!("HTTP status {}", res.status());
+        });
+
+    core.run(request).unwrap();
+}
+
 #[cfg(test)]
 mod test {
     use Config;
@@ -55,25 +78,4 @@ mod test {
         let token = Some("fakeToken".to_string());
         assert_eq!(Config::new(&args), Ok(Config { repo, token }));
     }
-}
-
-pub fn github_events(config: Config) {
-    let mut core = Core::new().unwrap();
-    let client = Client::configure()
-        .connector(HttpsConnector::new(4, &core.handle()).unwrap())
-        .build(&core.handle());
-
-    let url = "https://api.github.com/repos/".to_string() + &config.repo +
-        "/events?access_token=" + &config.token.unwrap_or("".to_string()) + "&page=1";
-    let mut req = Request::new(Method::Get, url.parse().unwrap());
-    req.headers_mut().set_raw("Accept", "application/vnd.github.v3+json");
-    println!("GitHub request: {:?}", &req);
-
-    let request = client
-        .request(req)
-        .map(|res| {
-            println!("HTTP status {}", res.status());
-        });
-
-    core.run(request).unwrap();
 }
