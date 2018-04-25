ef5fb22650ece07683ce3784ed7629b65bae4882
Nicolas Kosinski <nicokosi@yahoo.com>
2018-04-25 13:40:07 +0200

    Fix issue #4: 'commented' and 'closed' counters are incorrect

--- src/lib.rs
+++ src/lib.rs
@@ -60,42 +60,50 @@ pub fn github_events(config: Config) {
             let raw_events = raw_github_events(raw_events_as_json.to_string());
             let events_per_author: HashMap<String, Vec<RawEvent>> =
                 events_per_author(raw_events.unwrap());
-            println!("pull requests for {:?} ->", repo);
-            print!("\topened per author: {{ ");
-            for (author, events) in events_per_author.iter() {
-                let opened_pull_requests = events.into_iter()
-                    .filter(|e| {
-                        e.event_type == Type::PullRequestEvent
-                            && e.payload.action == Some(Action::opened)})
-                    .count();
-                print!("\"{}\" {} ", author, opened_pull_requests);
-            }
-            println!(" }}");
-            print!("\tcommented per author: {{ ");
-            for (author, events) in events_per_author.iter() {
-                let commented_pull_requests = events.into_iter()
-                    .filter(|e| {
-                        e.event_type == Type::PullRequestEvent
-                            && e.payload.action == Some(Action::closed)})
-                    .count();
-                print!("\"{}\" {} ", author, commented_pull_requests);
-            }
-            println!(" }}");
-            print!("\tclosed per author: {{ ");
-            for (author, events) in events_per_author.iter() {
-                let closed_pull_requests = events.into_iter()
-                    .filter(|e| {
-                        e.event_type == Type::PullRequestReviewCommentEvent
-                            && e.payload.action == Some(Action::created)})
-                    .count();
-                print!("\"{}\" {} ", author, closed_pull_requests);
-            }
-            println!("}}");
+            println!("{}", printable(repo, events_per_author));
             Ok(())
         })
     });
 
     core.run(work).unwrap();
+}
+
+fn printable(repo: String, events_per_author: HashMap<String, Vec<RawEvent>>) -> String {
+    let mut out: String = format!("pull requests for {:?} ->", repo);
+    out.push_str("\topened per author: { ");
+    for (author, events) in events_per_author.iter() {
+        let opened_pull_requests = events
+            .into_iter()
+            .filter(|e| {
+                e.event_type == Type::PullRequestEvent && e.payload.action == Some(Action::opened)
+            })
+            .count();
+        out.push_str(&format!("\"{}\" {}", author, opened_pull_requests));
+    }
+    out.push_str(" }");
+    out.push_str("\tcommented per author: { ");
+    for (author, events) in events_per_author.iter() {
+        let commented_pull_requests = events
+            .into_iter()
+            .filter(|e| {
+                e.event_type == Type::IssueCommentEvent && e.payload.action == Some(Action::created)
+            })
+            .count();
+        out.push_str(&format!("\"{}\" {}", author, commented_pull_requests));
+    }
+    out.push_str(" }");
+    out.push_str("\tclosed per author: { ");
+    for (author, events) in events_per_author.iter() {
+        let closed_pull_requests = events
+            .into_iter()
+            .filter(|e| {
+                e.event_type == Type::PullRequestEvent && e.payload.action == Some(Action::closed)
+            })
+            .count();
+        out.push_str(&format!("\"{}\" {}", author, closed_pull_requests));
+    }
+    out.push_str(" }");
+    return out.to_string();
 }
 
 use std::collections::HashMap;
@@ -104,7 +112,9 @@ fn events_per_author(events: Vec<RawEvent>) -> HashMap<String, Vec<RawEvent>> {
     return events
         .into_iter()
         .filter(|e| {
-            e.event_type == Type::PullRequestEvent || e.event_type == Type::PullRequestReviewCommentEvent
+            e.event_type == Type::PullRequestEvent
+                || e.event_type == Type::PullRequestReviewCommentEvent
+                || e.event_type == Type::IssueCommentEvent
         })
         .fold(HashMap::new(), |mut acc, event: RawEvent| {
             (*acc.entry(event.actor.login.clone()).or_insert(Vec::new())).push(event);
@@ -270,6 +280,87 @@ mod test {
             raw_github_events(include_str!("../test/github_events.json").to_string()).unwrap(),
         );
         assert!(events.get("alice").into_iter().len() == 1);
+    }
+
+    use std::collections::HashMap;
+    use printable;
+
+    #[test]
+    fn printable_with_opened_pull_request() {
+        let mut events: HashMap<String, Vec<RawEvent>> = HashMap::new();
+        events.insert(
+            "alice".to_string(),
+            vec![
+                RawEvent {
+                    actor: Actor {
+                        login: "alice".to_string(),
+                    },
+                    payload: Payload {
+                        action: Some(Action::opened),
+                    },
+                    event_type: Type::PullRequestEvent,
+                    created_at: Utc.ymd(2016, 12, 1).and_hms(16, 26, 43),
+                },
+            ],
+        );
+
+        let out = printable("repo".to_string(), events);
+
+        assert!(out.contains("opened per author: { \"alice\" 1 }"));
+        assert!(out.contains("commented per author: { \"alice\" 0 }"));
+        assert!(out.contains("closed per author: { \"alice\" 0 }"));
+    }
+
+    #[test]
+    fn printable_with_commented_pull_request() {
+        let mut events: HashMap<String, Vec<RawEvent>> = HashMap::new();
+        events.insert(
+            "alice".to_string(),
+            vec![
+                RawEvent {
+                    actor: Actor {
+                        login: "alice".to_string(),
+                    },
+                    payload: Payload {
+                        action: Some(Action::created),
+                    },
+                    event_type: Type::IssueCommentEvent,
+                    created_at: Utc.ymd(2016, 12, 1).and_hms(16, 26, 43),
+                },
+            ],
+        );
+
+        let out = printable("repo".to_string(), events);
+
+        assert!(out.contains("opened per author: { \"alice\" 0 }"));
+        assert!(out.contains("commented per author: { \"alice\" 1 }"));
+        assert!(out.contains("closed per author: { \"alice\" 0 }"));
+    }
+
+    #[test]
+    fn printable_with_closed_pull_request() {
+        let mut events: HashMap<String, Vec<RawEvent>> = HashMap::new();
+        events.insert(
+            "alice".to_string(),
+            vec![
+                RawEvent {
+                    actor: Actor {
+                        login: "alice".to_string(),
+                    },
+                    payload: Payload {
+                        action: Some(Action::closed),
+                    },
+                    event_type: Type::PullRequestEvent,
+                    created_at: Utc.ymd(2016, 12, 1).and_hms(16, 26, 43),
+                },
+            ],
+        );
+
+        let out = printable("repo".to_string(), events);
+
+        assert!(out.contains("opened per author: { \"alice\" 0 }"));
+        assert!(out.contains("commented per author: { \"alice\" 0 }"));
+        assert!(out.contains("closed per author: { \"alice\" 1 }"));
     }
 
 }
