91ee597872222b1207f3a22764ada22b169f7c4f
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-12-18 19:32:18 +0100

    Fix Clippy lints "warning: ... function could have a #[must_use] attribute" 🎨
    Seen with:
    
        cargo clippy -- -W clippy::pedantic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use log::info;
-
 use std::env;
 
 fn main() {
-    env_logger::init();
-    let config = pullpito::config_from_args(env::args_os().collect());
-    info!(
-        "Computing stats for GitHub repos '{:?}' (with token: {})",
-        config.repos,
-        config.token.is_some()
-    );
-    pullpito::github_events(config);
+    pullpito::log_github_events(env::args_os().collect());
 }
